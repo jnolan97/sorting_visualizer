80f16fd5cdface00fc327146f748b774846b94ae
jnolan97 <johnmnolan97@gmail.com>
2020-07-27 15:23:51 -0500

    logs to the system each swap

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-const bubbleSort = (array) => {
+let array = [5,4,1,2]
+async function bubbleSort() {
     for(let i = 0; i < array.length; i++){
         for(let j = 0; j < array.length - 1; j++){
             if(array[j] > array[j+1]){
-                swapping(array,j,j+1)
+                await swapping(array,j,j+1)
+                console.log(array)
             }
         }
-    }console.log(array)
+    } //console.log(array)
 }
 
-const swapping = (array,i,j) => {
+async function swapping(array,i,j) {
+    
     let temp = array[i]
     array[i]= array[j]
     array[j] = temp
+    await sleep(2000)
     
+}
+
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve,ms))
 }
